1a308faa16a1719eb1d2b9693e6fb2e91385f2a8
qetuop91 <983480256@qq.com>
2024-04-11 18:16:50 +0800

    更新博客代码

---
+++ SpringBootBlogWeb/sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      FeliksBlogManagementSystem
+    </div>
+    <div>
+      这是我的博客管理系统
+    </div>
+    <div>
+      微信id:Feliks13609777573
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
